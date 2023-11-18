f18bc95b3c66ea0d55932a9c6675b921466613aa
Davey <146002230+BirdDroppings@users.noreply.github.com>
2023-11-18 19:43:49 +0000

    working on json files again

---
+++ web class folder/web Project/javascript/script.js
@@ -0,0 +1,13 @@
+var basketItems =
+{//open basketItems
+   
+        "basket": //open
+    {//open basket
+        "basketID": 1,
+        "itemName": "",
+        "itemCategory": "",
+        "itemPrice": 0,
+        "itemQuantity": 0
+		}//close basket
+   
+	}//close basketItems
