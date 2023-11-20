13dbf61d2cb3dd614139650ee8c33a8f6ecc5612
Davey <146002230+BirdDroppings@users.noreply.github.com>
2023-11-20 16:39:28 +0000

    testing code

---
+++ webclass 12 jquery/main.js
@@ -0,0 +1,11 @@
+var checkedValue;
+var radioButtons = document.getElementsByClassName("myRadioGroup");
+for (var i = 0; i < radioButtons.length; i++)
+{
+if (radioButtons[i].checked)
+{
+checkedValue = radioButtons[i].value;
+break;
+}
+}
+alert (checkedValue);
